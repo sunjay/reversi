9efc19adb6ec05d892824c2ac43808040a8247b3
Sunjay Varma <varma.sunjay@gmail.com>
2016-08-21 22:50:16 -0400

    Much more refined formatting

--- src/Reversi.hs
+++ src/Reversi.hs
@@ -2,6 +2,8 @@ module Reversi where
 
 import qualified Data.Sequence as S
 import Data.Sequence (Seq)
+import Data.List (intercalate)
+import Data.Foldable (toList)
 
 -- Row and Col indexes start at 0
 type Row = Int
@@ -32,16 +34,29 @@ new = set (middle', middle') PieceX $
 
 -- | Sets the given row and column index to the given piece and returns a new game
 set :: (Row, Col) -> Piece -> Reversi -> Reversi
-set (row, col) = S.update (row * size + col) . Just
+set pos = S.update (_index pos) . Just
+
+-- | Gets the piece at the given row and column index
+get :: (Row, Col) -> Reversi -> Maybe Piece
+get pos tiles = S.index tiles $ _index pos
+
+-- | Returns the raw Seq index for a given row and col
+_index :: (Row, Col) -> Int
+_index (row, col) = row * size + col
 
 format :: Reversi -> String
-format = S.foldlWithIndex formatter ""
+format tiles = columnRow ++ formatRows tiles
     where
-        formatter acc i tile =
-            acc ++ " | " ++ (formatTile tile) ++ (lineEnding i)
+        columnRow = (intercalate sep $ cell " " : (map (\c -> cell [c]) $ take size ['A'..'Z'])) ++ sep ++ "\n" ++ divider
+        formatRows tiles = concatMap formatRow $ zip [0..] $ rows $ toList tiles
+        rows tiles = map (\i -> take size $ drop (i * size) tiles) [0..size-1]
+        formatRow (i, row) = (intercalate sep $ rowNumber i : (map (cell . formatTile) row)) ++ sep ++ "\n" ++ divider
         formatTile Nothing = " "
         formatTile (Just piece) = show piece
-        lineEnding index
-            | succ index `rem` size == 0 = " |\n"
-            | otherwise = ""
+        cell content = " " ++ content ++ " "
+        cellWidth = 3
+        rowNumber index = cell $ show $ succ index
+        sep = "|"
+        -- the succ and +1 are because of the extra row number column
+        divider = (take (succ size * cellWidth + size + 1) $ repeat '-') ++ "\n"
 
