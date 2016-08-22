9cdcd22dee49f37bb7ea3ed9704887a1b05b73a1
Sunjay Varma <varma.sunjay@gmail.com>
2016-08-22 02:29:45 -0400

    Implementation of diagonals, compiles but throws a runtime exception

--- src/Reversi.hs
+++ src/Reversi.hs
@@ -62,6 +62,26 @@ cols game = map col [0..size-1]
     where col index = S.fromList $ map (S.index tiles') [index,index+size..length tiles'-1]
           tiles' = tiles game
 
+-- | Returns all diagonals of every size and direction
+diagonals :: Reversi -> [Tiles]
+diagonals game = map diagonalTLBR colsStartIndexes ++ map diagonalTLBR rowsStartIndexes ++ map diagonalTRBL colsStartIndexes ++ map diagonalTRBL rowsStartIndexes
+    -- You can derive these formulas by drawing out a matrix of the indexes
+    -- and noticing that top-left to bottom-right (TLBR) diagonals go up
+    -- by indexes of size + 1 and top-right to bottom-left (TRBL) diagonals
+    -- go up by indexes of size - 1 with (size - start - 1) starting index
+    where diagonalTLBR start = pickIndexes $ map (\i -> start + i * (size + 1)) [0..diagSize start]
+          diagonalTRBL start = pickIndexes $ map (\i -> size - start + i * (size - 1)) [0..diagSize start]
+          pickIndexes indexes = S.fromList $ map (S.index tiles') indexes
+          tiles' = tiles game
+          diagSize start = size - start
+          -- Start indexes in the first row
+          colsStartIndexes = [0..size-minimumSize]
+          -- Start indexes in the first column
+          -- No need to get the first diagonal again (so start at 1)
+          rowsStartIndexes = map (*size) [1..size-minimumSize]
+          -- A diagonal less than this size isn't worth checking
+          minimumSize = 3
+
 -- | Sets the given row and column index to the given piece and returns a new game
 set :: (Row, Col) -> Piece -> Reversi -> Reversi
 set pos piece game = game {
@@ -80,7 +100,7 @@ format :: Reversi -> String
 format game = columnRow ++ formatRows game
     where
         columnRow = (intercalate sep $ cell " " : (map (\c -> cell [c]) $ take size ['A'..'Z'])) ++ sep ++ "\n" ++ divider
-        formatRows game' = concatMap formatRow $ zip [0..] $ rows game'
+        formatRows game' = concatMap formatRow $ zip [0..size] $ rows game'
         formatRow (i, row) = (intercalate sep $ rowNumber i : (map (cell . formatTile) (toList row))) ++ sep ++ "\n" ++ divider
         formatTile Nothing = " "
         formatTile (Just piece) = show piece
