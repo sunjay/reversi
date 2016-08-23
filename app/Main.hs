cf170b70f661161fa774388b295309c1fefec651
Sunjay Varma <varma.sunjay@gmail.com>
2016-08-23 02:17:01 -0400

    Removing EOF check because it was blocking

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ module Main where
 import Data.List (elemIndex)
 import Data.Maybe (isNothing, fromJust)
 import Text.Read (readMaybe)
-import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering), isEOF)
+import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
 
 import System.Console.ANSI (
     setSGR,
@@ -21,6 +21,7 @@ main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
     putStrLn $ R.format R.new
+    putStrLn ""
     play R.new
 
 play :: Reversi -> IO ()
@@ -31,27 +32,24 @@ play game = do
     putStrLn $ "The current piece is: " ++ (show $ R.currentPiece game)
 
     !maybeMove <- getMove
-    eof <- isEOF
-    if eof then do
-        -- Put an extra line to make sure the terminal's prompt is well aligned
-        putStrLn ""
-        return ()
-    else do
-        case makeMove game maybeMove of
-            Left error' -> do
-                putStrLn $ R.format game
+    case makeMove game maybeMove of
+        Left error' -> do
+            putStrLn $ R.format game
 
-                setSGR [SetColor Foreground Vivid Red]
-                putStrLn error'
-                setSGR []
-                play game
+            setSGR [SetColor Foreground Vivid Red]
+            putStrLn error'
+            setSGR []
+            play game
 
-            Right game' -> do
-                if null $! R.validMoves game' then do
-                    putStrLn ""
-                    return ()
-                else do
-                    play game'
+        Right game' -> do
+            if null $ R.validMoves game' then do
+                putStrLn ""
+                return ()
+            else do
+                putStrLn $ R.format game'
+                -- This empty line is placed where the error line would go
+                putStrLn ""
+                play game'
 
 getMove :: IO (Maybe (Row, Col))
 getMove = do
@@ -62,26 +60,26 @@ getMove = do
         return Nothing
     else do
         -- Note that the row number is not the same as the row index!
-        let maybeRow = readMaybe (take 1 line) :: Maybe Row
-            maybeCol = elemIndex (last line) validColumns
+        let maybeRow = readMaybe (drop 1 line) :: Maybe Row
+            maybeCol = elemIndex (head line) validColumns
 
         if isNothing maybeRow || (isNothing maybeCol) then do
             return Nothing
         else do
             return $ Just (pred $ fromJust maybeRow, fromJust maybeCol)
 
+prompt :: String -> IO String
+prompt message = do
+    putStr message
+    getLine
+
 validColumns :: [Char]
 validColumns = take R.size ['A'..'Z']
 
 makeMove :: Reversi -> Maybe (Row, Col) -> Either String Reversi
-makeMove _ Nothing = Left "Invalid move format. Enter something like '1A'."
+makeMove _ Nothing = Left "Invalid move format. Enter something like 'A1'."
 makeMove game (Just move) =
     if elem move $ R.validMoves game then
         Right $ R.move move game
     else
         Left "Invalid move. Your move must flip at least one tile."
-
-prompt :: String -> IO String
-prompt message = do
-    putStr message
-    getLine
