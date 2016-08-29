15264ffc942e358c38a61a61137f26088ca92406
Sunjay Varma <varma.sunjay@gmail.com>
2016-08-29 19:24:13 -0400

    Fixed a potential crash where Negamax code wasn't accounting for how lastMove can be Nothing when a turn is skipped

--- src/Reversi/AI/Negamax.hs
+++ src/Reversi/AI/Negamax.hs
@@ -16,16 +16,16 @@ import qualified Reversi.AI.GameTree as GT
 
 -- | Gets a move that the currentPiece should make for the given situation
 negamax :: AI
-negamax _ game = snd $ negamax' targetDepth (R.currentPiece game) $ gameTree game
+negamax _ game = fromJust $ snd $ negamax' targetDepth (R.currentPiece game) $ gameTree game
     where targetDepth = 5 -- how many moves deep to think
 
-negamax' :: Int -> Piece -> GameTreeNode -> (Integer, (Row, Col))
+negamax' :: Int -> Piece -> GameTreeNode -> (Integer, Maybe (Row, Col))
 negamax' depth player tree
-    | (depth == 0) || (null children) = (sign * (score player game), fromJust $ R.lastMove game)
+    | (depth == 0) || (null children) = (sign * (score player game), R.lastMove game)
     | otherwise = maximumBy compareMoves $ map search children
     where sign = if R.currentPiece game == player then 1 else -1
           game = GT.game tree
-          search child = (negate $ fst $ negamax' (pred depth) player child, fromJust $ R.lastMove $ GT.game child)
+          search child = (negate $ fst $ negamax' (pred depth) player child, R.lastMove $ GT.game child)
           compareMoves (s1, _) (s2, _) = compare s1 s2
           children = GT.children tree
 
